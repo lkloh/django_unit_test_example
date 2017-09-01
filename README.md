8ddbd005779c3795ec87a901310b3be9ea31ad01
LK Loh <lkloh2410@gmail.com>
2017-09-01 08:26:51 -0700

    Edited test to work

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # django_unit_test_example
 
 Running the site: `python manage.py runserver`
+
+## Credentials 
+
+* Username: `admin`
+* Email: `admin@example.com`
+* Password: `django2017`
+
+## Tests
+
+[doc](https://docs.djangoproject.com/en/1.11/topics/testing/tools/#django.test.TestCase)
+
+Run `python manage.py test polls`
--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import datetime
+
 from django.db import models
 from django.utils import timezone
 
@@ -13,7 +15,8 @@ class Question(models.Model):
 		return self.question_text
 
 	def was_published_recently(self):
-		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+		time_now = timezone.now()
+		return time_now - datetime.timedelta(days=1) <= self.pub_date <= time_now
 
 
 class Choice(models.Model):
